613eb3db819904daddd7a408024a554ced131168
WindProphet <lihanyuan1996@gmail.com>
2022-02-19 07:45:42 +0900

    script: add more argument option to script

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 set -e
-set -x
 
-CWD="$(pwd)"
-
-function error_handler() {
-  set +x
-  echo "we meet an error when installing" >&2
-  echo "remove FFmpeg folder" >&2
-  rm -r "$CWD/FFmpeg"
-  exit 1
-}
-
-trap error_handler ERR
-trap error_handler INT
-
+CLEAN_ON_FAILED=YES
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CWD="$SCRIPT_DIR"
 VERSION=5.0
 FFMPEGFLAGS=(
   --enable-gpl
@@ -33,15 +22,84 @@ FFMPEGFLAGS=(
   --disable-appkit
 )
 
-if which yasm || which nasm; then
-  true
-else
+usage() {
+  cat<<USAGE
+Script for building FFmpeg library
+
+  $0 [--no-clean-on-failed] [--universal]
+
+  -h, --help              show this help
+  --universal             build universal library
+  --[no-]clean-on-failed  remove FFmpeg folder if any error happened
+  -f=FLAG, --flag=FLAG    add ffmpeg flag when build configuration
+  --cwd=DIR               set compiling folder
+  -c, --clean             clean up FFmpeg folder
+  --prefix=PREFIX         FFmpeg library install path
+
+USAGE
+}
+
+for i in "$@"; do
+  case $i in
+    --universal)
+      UNIVERSAL=YES
+      ;;
+    --prefix=*)
+      PREFIX="${i#*=}"
+      ;;
+    --clean-on-failed)
+      CLEAN_ON_FAILED=YES
+      ;;
+    --no-clean-on-failed)
+      CLEAN_ON_FAILED=NO
+      ;;
+    -f=*|--flag=)
+      FFMPEGFLAGS+=("${i#*=}")
+      ;;
+    --cwd=*)
+      cd "${i#*=}"
+      CWD="$(pwd)"
+      ;;
+    -c|--clean)
+      rm -r "$CWD/FFmpeg"
+      exit 1
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    -*)
+      echo "Unknown option $i"
+      usage
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [[ $CLEAN_ON_FAILED = YES ]]; then
+  function error_handler() {
+    set +x
+    echo "we meet an error when installing" >&2
+    echo "remove FFmpeg folder" >&2
+    rm -r "$CWD/FFmpeg"
+    exit 1
+  }
+  trap error_handler ERR
+  trap error_handler INT
+fi
+
+if [[ ! $(which yasm) && ! $(which nasm) ]]; then
   FFMPEGFLAGS+=(--disable-x86asm)
 fi
 
 mkdir -p FFmpeg/src
-curl https://ffmpeg.org/releases/ffmpeg-$VERSION.tar.bz2 -o FFmpeg/ffmpeg-$VERSION.tar.bz2
+curl -C - https://ffmpeg.org/releases/ffmpeg-$VERSION.tar.bz2 -o FFmpeg/ffmpeg-$VERSION.tar.bz2
 tar -xf FFmpeg/ffmpeg-$VERSION.tar.bz2 -C FFmpeg/src
-cd FFmpeg/src/ffmpeg-$VERSION
-./configure "${FFMPEGFLAGS[@]}" --prefix=../..
+
+pushd FFmpeg/src/ffmpeg-$VERSION
+make clean || true
+./configure "${FFMPEGFLAGS[@]}" --prefix=../../"$PREFIX"
 make -j8 install
+popd
